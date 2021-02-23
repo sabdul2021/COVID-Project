a43b823bec1269fef874f89d64200c760d69656d
Sammy Abdulkader <sammyabdulkader@gmail.com>
2021-02-22 19:08:39 -0500

    added the credentials file

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var http = require("http");
 var fs = require("fs");
 var mysql = require("mysql");
 var qs = require("querystring");
-var credentials = require("./credentials");
+var credentials = require("views/js/credentials");
 
 http.createServer(function(req, res) {
   try {
---
+++ views/js/credentials.js
@@ -0,0 +1,8 @@
+module.exports = {
+  connection: {
+    host     : "sabdul.it.pointpark.edu",
+    user     : "sabdul",
+    password : "7bEuNPU6",
+    database : "Covid_Checker"
+  }
+};
