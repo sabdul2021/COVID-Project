9b562b94a3754479037b60bb0c7a5b7e80379c8c
Sammy Abdulkader <sammyabdulkader@gmail.com>
2021-02-28 22:13:56 -0500

    added additional code

--- app.js
+++ app.js
@@ -63,6 +63,42 @@ function serveStaticFile(res, path, contentType, responseCode) {
   });
 }
 
+function serveStaticAboutFile(res, path, contentType, responseCode) {
+  if (!path) path = "/about.html";
+  if (!responseCode) responseCode = 200;
+  if (!contentType) {
+    contentType = "application/octet-stream";
+    if (path.endsWith("about.html")) {
+      contentType = "text/html; charset=utf-8";
+    } else if (path.endsWith("home.html")){
+      contentType = "text/html; charset=utf-8";
+    } else if (path.endsWith(".js")) {
+      contentType = "application/javascript; charset=utf-8";
+    } else if (path.endsWith(".json")) {
+      contentType = "application/json; charset=utf-8";
+    } else if (path.endsWith(".css")) {
+      contentType = "text/css; charset=utf-8";
+    } else if (path.endsWith(".png")) {
+      contentType = "image/png";
+    } else if (path.endsWith(".jpg")) {
+      contentType = "text/jpeg";
+    }
+  }
+  fs.readFile(__dirname + "/views" + path, function(err, data) {
+    if (err) {
+      res.writeHead(404, {
+        "Content-Type": "text/plain; charset=utf-8"
+      });
+      res.end("404 Not Found");
+    } else {
+      res.writeHead(200, {
+        "Content-Type": contentType
+      });
+      res.end(data);
+    }
+  });
+}
+
 function sendResponse(req, res, data) {
   res.writeHead(200, {
     "Content-Type": "application/json; charset=utf-8"
