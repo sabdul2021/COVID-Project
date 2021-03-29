0e33316e1f001b4c560e8c79590a648b332e0d4c
Sammy Abdulkader <sammyabdulkader@gmail.com>
2021-03-29 18:59:05 -0400

    adding the fix changes

--- views/js/main.js
+++ views/js/main.js
@@ -170,3 +170,31 @@ function showLineGraph() {
             });
     }
 }
+
+let ctx = document.getElementById('canvas').getContext('2d');
+let canvas = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                }
+            }]
+        }
+    }
+});
